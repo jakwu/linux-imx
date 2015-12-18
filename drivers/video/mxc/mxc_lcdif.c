9a951aadc847dcb56ced5dab5cfae2f0747153e3
jakwu <you@example.com>
2015-12-18 15:26:14 +0100

    nanoRISC support.

--- drivers/video/mxc/mxc_lcdif.c
+++ drivers/video/mxc/mxc_lcdif.c
@@ -48,6 +48,12 @@ static struct fb_videomode lcdif_modedb[] = {
 	FB_SYNC_CLK_LAT_FALL,
 	FB_VMODE_NONINTERLACED,
 	0,},
+	{
+	/* 800x480 @ 60 Hz , pixel clk @ 33MHz */
+	"AMPIRE-WVGA",	60, 800, 480, 33264, 88, 88, 10, 10, 80, 25,
+	FB_SYNC_HOR_HIGH_ACT | FB_SYNC_VERT_HIGH_ACT,
+	FB_VMODE_NONINTERLACED,
+	0,},
 };
 static int lcdif_modedb_sz = ARRAY_SIZE(lcdif_modedb);
 
